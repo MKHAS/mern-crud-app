cac24d666f71a2b221e6a01ed5a056d7e8f1f736
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2023-10-23 17:08:48 +0300

    Add frontend code using   create-react-app <project-directory>
    For example:
      create-react-app my-react-app
    
    Run create-react-app --help to see all options.

--- server/server.js
+++ server/server.js
@@ -7,12 +7,14 @@ if (process.env.NODE_ENV != 'production') {
 const express = require("express");
 const connectToDb = require("./config/connectToDb");
 const notesController = require("./controllers/notesController");
+const cors = require("cors");
 
 // Create an express App
 const app = express();
 
-// configure express to read json
+// configure express to read json and use cors
 app.use(express.json());
+app.use(cors());
 
 // Connect to Database
 connectToDb();
