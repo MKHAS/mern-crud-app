dc3c7045cf21b629077e3b6c2014a2df4761e6f6
Mohamad-Kazem Abd-el-Sater <mkhas313@gmail.com>
2023-10-23 22:36:38 +0300

    Add the ability to create and delete notes in the form (too many changes :C)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,96 @@
+import { useEffect, useState } from "react";
+import axios from 'axios';
+
 function App() {
+  // State
+  const [notes, setNotes] = useState(null);
+  const [createForm, setCreateForm] = useState({
+    title: "",
+    body: "",
+  });
+
+  // Use Effect
+  useEffect(() => {
+    fetchNotes();
+  }, [])
+
+
+  // Functions
+  const fetchNotes = async () => {
+    // fetch notes
+    const res = await axios.get('http://localhost:3000/notes');
+    // set to state
+    setNotes(res.data.notes);
+    console.log(res);
+  };
+
+  const updateCreateFormField = (e) => {
+    const {name, value} = e.target;
+
+    setCreateForm({
+      ...createForm, // this creates a duplicate of the createForm object
+      [name]: value
+    })
+
+  };
+
+  const createNote = async (e) => {
+    e.preventDefault(); //prevent the page from refreshing when we submit form
+
+    //create the note
+    const res = await axios.post("http://localhost:3000/notes", createForm);
+
+    //update state
+    setNotes([...notes, res.data.note]);
+
+    // clear form state
+    setCreateForm({ title: "", body: ""});
+
+  };
+
+  const deleteNote = async (_id) => {
+    //delete note 
+    const res = await axios.delete(`http://localhost:3000/notes/${_id}` );
+    console.log(res);
+    //update state
+    const newNotes = [...notes].filter(note => {
+      return note._id !== _id;
+    });
+
+    setNotes(newNotes);
+  }
+
   return (
     <div className="App">
-      Sample Text
+      <div>
+        <h2>Notes: </h2>
+        {notes &&
+         notes.map((note) => {
+          return (
+            <div key={note._id} >
+              <h3>{note.title}</h3>
+              
+              <button onClick={() => { deleteNote(note._id)}}>Delete Note</button>
+            </div>);
+        })}
+      </div>
+
+      <div>
+        <h2>Create Note</h2>
+        <form onSubmit={createNote}>
+          <input
+           onChange={updateCreateFormField}
+           value={createForm.title}
+           name="title"
+           />
+          <textarea 
+          onChange={updateCreateFormField} 
+          value={createForm.body} 
+          name="body"
+          />
+          <button type="submit"> Create Note </button>
+        </form>
+      </div>
     </div>
   );
 }
